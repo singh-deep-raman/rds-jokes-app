c95d2b2c9e86cbd830856d9e6a73e0f3c94fd190
singh-deep-raman <raman111092@gmail.com>
2020-09-13 03:56:28 +0530

    initial project created

---
+++ src/main/java/com/raman/jokes/chucknorre/ChuckNorreApplication.java
@@ -0,0 +1,13 @@
+package com.raman.jokes.chucknorre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raman/jokes/chucknorre/ChuckNorreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raman.jokes.chucknorre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
