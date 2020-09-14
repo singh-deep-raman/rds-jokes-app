861111a719c6bc299248084444219cc00fcc662b
singh-deep-raman <raman111092@gmail.com>
2020-09-14 23:25:24 +0530

    xml based beans - lesson #74

--- src/main/java/com/raman/jokes/chucknorre/ChuckNorreApplication.java
+++ src/main/java/com/raman/jokes/chucknorre/ChuckNorreApplication.java
@@ -2,8 +2,10 @@ package com.raman.jokes.chucknorre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:beans-config.xml") // it tells Spring to load beans from this file too
 public class ChuckNorreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/raman/jokes/chucknorre/config/ChuckConfiguration.java
+++ src/main/java/com/raman/jokes/chucknorre/config/ChuckConfiguration.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /** Spring will read this annotation and create bean of it in it's context */
-@Configuration
+//@Configuration - commenting to show demo for xml based bean
 public class ChuckConfiguration {
 
     /** now it is handy to get this bean injected anywhere we need in the project */
-    @Bean
+    // @Bean - commenting to show demo for xml based bean
     public ChuckNorrisQuotes getChuckNorrisQuotesBean() {
         return new ChuckNorrisQuotes();
     }
